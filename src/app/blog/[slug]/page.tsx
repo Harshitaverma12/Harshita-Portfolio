b3ba3c4608b2b2a49fc231012f72509019e2bffd
HARSHITA VERMA <harshitaverma0526@hmail.com>
2025-05-21 11:51:18 +0530

    changes

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -23,9 +23,7 @@ export async function generateMetadata({
     title,
     publishedAt: publishedTime,
     summary: description,
-    image,
   } = post.metadata;
-  let ogImage = image ? `${DATA.url}${image}` : `${DATA.url}/og?title=${title}`;
 
   return {
     title,
@@ -35,18 +33,13 @@ export async function generateMetadata({
       description,
       type: "article",
       publishedTime,
-      url: `${DATA.url}/blog/${post.slug}`,
-      images: [
-        {
-          url: ogImage,
-        },
-      ],
+      
     },
     twitter: {
       card: "summary_large_image",
       title,
       description,
-      images: [ogImage],
+     
     },
   };
 }
@@ -77,10 +70,7 @@ export default async function Blog({
             datePublished: post.metadata.publishedAt,
             dateModified: post.metadata.publishedAt,
             description: post.metadata.summary,
-            image: post.metadata.image
-              ? `${DATA.url}${post.metadata.image}`
-              : `${DATA.url}/og?title=${post.metadata.title}`,
-            url: `${DATA.url}/blog/${post.slug}`,
+         
             author: {
               "@type": "Person",
               name: DATA.name,
